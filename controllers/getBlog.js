d732a45097d8c320c377d51f3fb0c8441160769f
Mark-Nelovka <87484591+Mark-Nelovka@users.noreply.github.com>
2022-09-08 18:43:30 +0300

    add images in JSON

--- controllers/getBlog.js
+++ controllers/getBlog.js
@@ -1,7 +1,7 @@
-const { Blogs } = require("../models/Blog");
+const Blogs = require("../models/Blog");
 
 const getBlog = async (req, res) => {
-  const result = await Blogs.findById("6319c258710b70dcbd2a66f8");
+  const result = await Blogs.findById("6319f6dabc48921c93b1fd23");
   res.json(result);
   res.json({ result: "done" });
 };
