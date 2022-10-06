9dbe7520f6a36f259fd318aadc8001b5ac45cb61
Dima Malik <45137601+salat4@users.noreply.github.com>
2022-10-06 21:11:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,4 @@ app.use("/createx/about", aboutRouter);
 app.use("/createx/auth", authRouter);
 let port_number = server.listen(process.env.PORT || 3000);
 app.listen(port_number);
-app.listen(process.env.PORT || 3000, function () {
-  console.log("CORS-enabled web server listening on port 3000");
-});
+
